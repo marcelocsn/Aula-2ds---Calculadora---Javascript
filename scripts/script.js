63fc981e4ed744ac72e3b25e6f6ce5c41a09abf5
Marcelo Nicolela <csnicolela.marcelo@gmail.com>
2025-08-26 12:20:18 -0300

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,15 @@
+const screen = document.getElementById('answer');
+
+const buttons = document.querySelectorAll('.calc-btn');
+
+const historyBtn = document.getElementById('history-btn');
+
+const historyPanel = document.getElementById('history-panel');
+
+const historyList = document.getElementById('history-list');
+
+const closeHistory = document.getElementById('close-history');
+
+const clearHistory = document.getElementById('clear-history');
+
+const themeToggle = document.getElementById('theme-toggle');
